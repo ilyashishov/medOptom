29d4511e179c6740500a0a3daff43f856ab3828d
IlyaShishov <stalk1258@yandex.ru>
2016-06-02 10:47:04 +0300

    new commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,33 @@ var gulp = require("gulp");
 var less = require('gulp-less');
 var path = require('path');
 var imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+
+
+gulp.task('minifyJS', ['js'], function (cb) {
+  pump([
+        gulp.src('./js/**/*.js'),
+        uglify(),
+        gulp.dest('./js/')
+    ],
+    cb
+  );
+});
+
 
 gulp.task('watch', function() {
-    gulp.watch('./css/main.less', ['minify-css']);
+    gulp.watch('./css/**/*.less', ['minify-css']);
+    gulp.watch('./javascript/**/*.js', ['js']);
 });
 
 gulp.task('images', function() {
     return gulp.src('./img/**/*')
             .pipe(imagemin({optimizationLevel: 5}))
-            .pipe(gulp.dest('./img'));
+            .pipe(gulp.dest('./img2'));
 });
 
 gulp.task('less', function () {
@@ -28,4 +45,12 @@ gulp.task('minify-css', ['less'],  function() {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', ['watch', 'images', 'minify-css']);
+gulp.task('js', () => {
+  return gulp.src('./javascript/**/*')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('default', ['watch', 'images', 'minify-css', 'js']);
